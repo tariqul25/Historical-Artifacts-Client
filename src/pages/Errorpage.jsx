b41996966f31f9f96eaaa9fcf407bc603fe17779
ImTariquL <imtariqul21@gmail.com>
2025-08-10 10:26:53 +0600

    error page update

--- src/pages/Errorpage.jsx
+++ src/pages/Errorpage.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router';
+import animationData from '../../src/assets/lotties/error.json'
+import Lottie from 'lottie-react';
 
 const ErrorPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-amber-50 via-white to-orange-50 px-4">
       <div className="text-center max-w-md">
-        <h1 className="text-4xl font-bold mb-4">Oops!</h1>
-        <p className="text-3xl  mb-6">Page not found</p>
+         <Lottie  animationData={animationData} loop={true} className="" />
         <Link to='/'> <button
           className="text-amber-600 btn btn-outline    text-lg"
         >Return to Home</button></Link>
-
-
       </div>
     </div>
   );
